84dd8ab8547c09d64c74dce18454f301d203653c
Esteban <estebanrg21@icloud.com>
2021-02-19 14:03:45 -0600

    Create delete_vhost.sh

---
+++ vhosts-wsl/delete_vhost.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+apache_file="/etc/apache2/sites-available/default-folder.conf"
+hosts_file="/mnt/c/Windows/System32/drivers/etc/hosts"
+
+#read -p "Enter name of vhost to delete:" vhost
+#read -p "Enter domain of vhost to delete:" domain
+if [ ! -z $1 ] || [ ! -z $2 ]; then
+	sed -i "/#"$1"/,/VirtualHost>/d" $apache_file
+	sed -i "s/ $1.$2//g" $hosts_file
+	rm .backupApacheConfig
+	rm .backupHostsFile
+	rm -r $1
+else
+	echo "name of vhost cannot be null"
+fi
+
+#must be executed under a wsl host ran as admin and with this syntax: sudo bash delete_vhost.sh folder_to_delete domain_of_vhost
